42c93cf671421baef448ce1437e253f829d70eef
sd <sd@atechlab.eu>
2019-10-14 18:02:54 +0300

    fix bug with no filename in mailserver.h

--- mailserver.sh
+++ mailserver.sh
@@ -7,7 +7,7 @@ chown -R nobody:nobody /var/archive
 postconf -e always_bcc=mailarchive@localhost
 echo "mailarchive: /var/archive/mail/" >> /etc/aliases
 newaliases
-sed -i '/inet_interfaces/ s/localhost/all/'
+grep "^inet_interfaces = all" /etc/postfix/main.cf || sed -i '/inet_interfaces/ s/localhost/all/' /etc/postfix/main.cf
 systemctl restart postfix
 #semanage fcontext -a -t user_home_dir_t "/var/archive(/.*)?"
 #restorecon -R /var/archive
